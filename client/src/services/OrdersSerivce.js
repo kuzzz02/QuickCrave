2a3e1d9154ae3bceaf3331c089b091b700fd3e90
kuzzz02 <1343275629@qq.com>
2024-06-06 19:15:48 +0800

    refine services and update service documents

--- client/src/services/OrdersSerivce.js
+++ client/src/services/OrdersSerivce.js
@@ -1,20 +1,41 @@
 import http from "../http";
 
 class OrdersService {
+  /**
+   * @param {Long} goods_id
+   * @param {long} user_id
+   * @param {Long} vendor_id
+   * @param {Long} delivery_id
+   * @param {*} state
+   * @param {*} date
+   * @param {*} address
+   * @param {*} phone
+   * @param {*} payment
+   * @param {*} total
+   */
+  create(goods_id,user_id,vendor_id,delivery_id,state,date,address,phone,payment,total) {
+    return http.post(`/orders/insert?goods_id=${goods_id}
+      &user_id=${user_id}
+      &vendor_id=${vendor_id}
+      &delivery_id=${delivery_id}
+      &state=${state}
+      &date=${date}
+      &address=${address}
+      &phone=${phone}
+      &payment=${payment}
+      &total=${total}`);
+  }
+
   getAll() {
     return http.get("/orders/selectAll");
   }
 
   getById(id) {
-    return http.get(`/orders/selectById/${id}`);
-  }
-
-  create(data) {
-    return http.post("/orders/insert", data);
+    return http.get(`/orders/selectById?id=${id}`);
   }
 
   delete(id) {
-    return http.delete(`/orders/delete/${id}`);
+    return http.delete(`/orders/delete?id=${id}`);
   }
 
   deleteAll() {
